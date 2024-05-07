7fd9de3fca3859347152788a1edbc3d08cdcf1a7
Divik Bhatia <111828505+Divik09102003@users.noreply.github.com>
2024-05-07 01:23:50 +0000

    completed

--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -2,6 +2,7 @@
 
 import { TaskItem } from "./task-item";
 import styles from "./task-list.module.css";
+import { useState } from "react";
 
 export type Task = {
   id: string;
@@ -10,15 +11,62 @@ export type Task = {
 };
 
 export function TaskList({ tasks }: { tasks: Task[] }) {
+  const [taskList, setTaskList] = useState<Task[]>(tasks);
+  const [newTaskValue, setTaskValue] = useState("");
+
+  const handleAddTask =() => {
+    if (newTaskValue==""){
+      return;
+    }
+  
+
+    const newTask: Task = {
+      id: Math.random().toString(),
+      title: newTaskValue,
+      state: "ACTIVE"
+    };
+    setTaskList([...taskList, newTask]);
+    setTaskValue("");
+  };
+
+    const handleDelete = (id: string) => {
+      const newTaskList = taskList.filter((task) => task.id !== id);
+      setTaskList(newTaskList);
+    }
+
+    const handleToggleCompleted = (id: string) => {
+      const newTaskList = taskList.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            state: (task.state === "ACTIVE" ? "COMPLETED" : "ACTIVE") as | 
+            "COMPLETED" | "ACTIVE" | "PINNED",
+          };
+        }
+        return task;
+      });
+      setTaskList(newTaskList);
+    }
+
+    const countActiveTask = (tasks: Task[]):number => {
+      return tasks.filter((task) => task.state === "ACTIVE").length;
+    };
+
+
+
   return (
     <>
       <div>
         <section className={styles.counter}>
-          <div className={styles.taskLabel}>0 tasks</div>
+          <div className={styles.taskLabel}>{countActiveTask(taskList) === 1 ? '1 task' : `${countActiveTask(taskList)} tasks`} </div>
         </section>
         <section className={styles.section}>
-          {tasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+          {taskList.map((task) => (
+            <TaskItem 
+            key={task.id} 
+            task={task}
+            handleDelete={handleDelete}
+            handleToggleCompleted={handleToggleCompleted} />
           ))}
         </section>
       </div>
@@ -27,9 +75,12 @@ export function TaskList({ tasks }: { tasks: Task[] }) {
           type="text"
           placeholder="What needs to be done?"
           className={styles.taskInput}
+          value = {newTaskValue}
+          onChange={(e) => setTaskValue(e.target.value)}
         />
-        <button className={styles.taskButton}>Add Task</button>
+        <button className={styles.taskButton} onClick={handleAddTask}>Add Task</button>
       </section>
     </>
   );
 }
+
